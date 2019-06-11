527b80a6523bfc18cfe1ab9f0e32ba4e6034b3b4
Caleah <caleah77@gmail.com>
2019-06-11 14:44:24 -0700

    finish itiration one

---
+++ ViewFinder/PhotoViewController.swift
@@ -0,0 +1,45 @@
+//
+//  PhotoViewController.swift
+//  ViewFinder
+//
+//  Created by Apple on 6/11/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func takePhoto(_ sender: UIButton) {
+    }
+    
+    @IBAction func FindPhoto(_ sender: UIButton) {
+    }
+   
+    @IBAction func goToAlbum(_ sender: UIButton) {
+    }
+    
+    @IBAction func savePhoto(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
